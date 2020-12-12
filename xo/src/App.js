d51c005c4e380ba43fc4203c47b3f673e1485169
IuriiKyrylovskyi <iurii712@ukr.net>
2020-12-12 02:53:05 +0200

    - html

--- xo/src/App.js
+++ xo/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,8 +17,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
+      </header> */}
+    
+	  </div>
   );
 }
 
